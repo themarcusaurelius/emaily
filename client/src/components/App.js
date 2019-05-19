1182952d31ffcfef79c717ffb73cdd59507ae43c
Marcus Aurelius <markmayfield16@gmail.com>
2019-05-19 13:24:49 -0500

    Header Styling Added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,26 @@
 //Rendering layer control (React)
 import React from 'react';
+//Browser Router - Brain of React-Router
+import { BrowserRouter, Route} from 'react-router-dom';
+import Header from './Header';
 
-class App extends React.Component {
-    render() {
-        return (
-            <div>
-                Hi There!
-            </div>
-        );
-    }
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
+
+const App = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Header/>
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/surveys" component={Dashboard} />
+                    <Route path="/surveys/new" component={SurveyNew} />
+                </div>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default App;
