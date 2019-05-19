a7cd97cc966bf2bdb7cd8f1276d01979ffb57550
Marcus Aurelius <markmayfield16@gmail.com>
2019-05-19 11:59:18 -0500

    Auth Reducer Set Up

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,9 @@
+// Records whether or not the user is logged in
+
+//Authentication Reducer
+export default function(state = {}, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
