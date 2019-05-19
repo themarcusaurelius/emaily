baa11ed52d05be46a968dca8ad931ae004daf088
Marcus Aurelius <markmayfield16@gmail.com>
2019-05-19 11:53:58 -0500

    Redux Store Created and Wired Up

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+//Rendering layer control (React)
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                Hi There!
+            </div>
+        );
+    }
+};
+
+export default App;
