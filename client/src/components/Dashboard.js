4fac9c01231cd29f6b8b7c0d2bf0196a125853f2
Marcus Aurelius <markmayfield16@gmail.com>
2019-05-21 14:46:29 -0500

    Linked Tag On Dashboard Button

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,16 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Dashboard = () => {
     return (
         <div>
             Dashboard
             <div className="fixed-action-btn">
-                <a className="btn-floating btn-large waves-effect waves-light deep-purple lighten-2">
+                <Link to="/surveys/new" className="btn-floating btn-large waves-effect waves-light deep-purple lighten-2">
                     <i className="large material-icons">add</i>
-                </a>
+                </Link>
             </div>
         </div>
-        
     );
 };
 
