1ca55c7c549f547ac409a021d1e1f1eb4e78e731
Marcus Aurelius <markmayfield16@gmail.com>
2019-05-22 11:34:32 -0500

    lodash library added to map over field component

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,19 +1,25 @@
 //Shows a form for a user to add input
+import _ from 'lodash';
 import React, { Component } from 'react';
 //Field component is a helper for rendering any type of traditional form input
 import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField';
 
+//Capitol letters because it should never be changed
+const FIELDS = [
+    { label: 'Survey Title', name: 'title' },
+    { label: 'Subject Line', name: 'subject' },
+    { label: 'Email Body', name: 'body' },
+    { label: 'Recipient List', name: 'emails'}
+]
+
 class SurveyForm extends Component {
     renderFields() {
-        return (
-            <div>
-                <Field label="Survey Title" type="text" name="title" component={SurveyField} />
-                <Field label="Subject Line" type="text" name="subject" component={SurveyField} />
-                <Field label="Email Body" type="text" name="body" component={SurveyField} />
-                <Field label="Recipient List" type="text" name="emails" component={SurveyField} />
-            </div>
-        );
+        return _.map(FIELDS, ({ label, name }) => {
+            return (
+                <Field key={name} component={SurveyField} type="text" label={label} name={name} />
+            );
+        });
     };
 
     render() {
